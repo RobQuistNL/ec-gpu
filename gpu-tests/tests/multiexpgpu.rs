560a447ecc0f15dbe012c68d762310470cea1a0c
RobQuistNL <rob.quist@dukesoft.nl>
2022-09-14 14:17:18 +0200

    I'm a rust noob

--- gpu-tests/tests/multiexpgpu.rs
+++ gpu-tests/tests/multiexpgpu.rs
@@ -69,11 +69,11 @@ fn gpu_multiexp_consistency() {
                     .collect::<Vec<_>>(),
             );
 
-            for internalRep in 0..=REPEAT {
+            for internal_rep in 0..=REPEAT {
                 let now = Instant::now();
                 let _gpu = multiexp_gpu(&pool, (g.clone(), 0), FullDensity, v.clone(), &mut kern).unwrap();
                 let gpu_dur = now.elapsed().as_secs() * 1000 + now.elapsed().subsec_millis() as u64;
-                println!("GPU repetition {} took {}ms.", internalRep, gpu_dur);
+                println!("GPU repetition {} took {}ms.", internal_rep, gpu_dur);
             }
             println!("============================");
             if repetition == 0 {
